9bec6918e9167960f52c4476fb9242f39bff62ac
Jules7 <akj123429@gmail.com>
2019-04-20 14:52:10 +0200

    Initial commit

---
+++ include/mat.h
@@ -0,0 +1,17 @@
+//
+// Created by hp on 20.04.19.
+//
+
+#ifndef MATH_MAT_H
+#define MATH_MAT_H
+
+#include <math.h>
+
+
+extern double mySqrt(double value)
+{
+    return sqrt((double)value);
+}
+
+
+#endif //MATH_MAT_H
---
+++ test/test.c
@@ -0,0 +1,19 @@
+//
+// Created by hp on 20.04.19.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <math.h>
+
+#include "mat.h"
+
+int main()
+{
+
+    printf("%lf\n", mySqrt(9));
+
+
+    return EXIT_FAILURE;
+}
